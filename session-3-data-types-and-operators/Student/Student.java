b657632d901c30670fd42aa35da752786bec6d0f
fan <143589170+anotherphanie@users.noreply.github.com>
2023-10-07 14:11:27 +0700

    Create Student.java

---
+++ session-3-data-types-and-operators/Student/Student.java
@@ -0,0 +1,63 @@
+package Student;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private String ssn;
+    private double GPA;
+
+    // Constants should be private, static, and final
+    private static final int SCHCODE = 34958;
+
+    // Constructor to initialize a Student object
+    public Student(int studentID, String name, String ssn, double GPA) {
+        this.studentID = studentID;
+        this.name = name;
+        this.ssn = ssn;
+        this.GPA = GPA;
+    }
+
+    // Default constructor
+    public Student() {
+
+    }
+
+    // Getter methods
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    // Setter methods (optional)
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public String toString() {
+        return "Student ID: " + studentID + "\nName: " + name + "\nSSN: " + ssn + "\nGPA: " + GPA;
+    }
+
+}
