5978c2e40b65df7e5309deb769dc191e9602ef94
fan <143589170+anotherphanie@users.noreply.github.com>
2023-10-07 14:12:49 +0700

    Create StudentNewTester.java

---
+++ session-3-data-types-and-operators/StudentNew/StudentNewTester.java
@@ -0,0 +1,8 @@
+package StudentNew;
+
+public class StudentNewTester {
+    public static void main(String[] args) {
+        StudentNew s1 = new StudentNew("Caron", "Newman", 123456789, "Active");
+        System.out.println(s1);
+    }
+}
