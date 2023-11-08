1bb4e2803b3a8c4ca93db4d2b3dd4f0389604676
Phanie <101379066+lymphoidcell@users.noreply.github.com>
2023-11-08 03:00:59 +0000

    hayooo

--- session-9-inheritance/in-class-s9/Shape.java
+++ session-9-inheritance/in-class-s9/Shape.java
@@ -1,1 +1,75 @@
+class Shape {
+    protected String type;
+
+    public Shape(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double area() {
+        return 0.0;
+    }
+
+    public double perimeter() {
+        return 0.0;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        super("Rectangle");
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+}
+
+public class ShapeDemo {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Shape: " + circle.getType());
+        System.out.println("Area: " + circle.area());
+        System.out.println("Perimeter: " + circle.perimeter());
+
+        System.out.println("\nShape: " + rectangle.getType());
+        System.out.println("Area: " + rectangle.area());
+        System.out.println("Perimeter: " + rectangle.perimeter());
+    }
+}
 
